6bf521e83e214d91bc0cd00775b123e2060bfe8d
QuanLew <90435027+QuanLew@users.noreply.github.com>
2021-11-12 18:31:37 -0800

    Delete myapp directory

--- myapp/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-	city_name =  StringField('Username', validators=[DataRequired()])
-	city_rank = PasswordField('password')	
-	remember_me = BooleanField('Remember Me', default=False)
-	submit = SubmitField("Submit")
